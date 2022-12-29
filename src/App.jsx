57c063d1a2adce7119a5350aa396a417f297ee34
Tom Williams <77905483+willtonian1@users.noreply.github.com>
2022-12-29 22:47:23 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export default function App() {
+	return (
+		<main>
+			<div id="background">
+
+				<img id="logo" src="src/newlogo.png"></img>
+
+				<img id="overlay" src="src/overlay.png"></img>
+
+			</div>
+		</main>
+	)
+}
